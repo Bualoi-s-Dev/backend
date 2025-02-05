2bd6ebb24fb48d22867e79263c8e118e7d25fd9e
ThanaphomH <thanaphom.h@gmail.com>
2025-02-05 21:55:30 +0700

    feat: gen folder for generated files

--- cmd/tsgen/tsgen.go
+++ cmd/tsgen/tsgen.go
@@ -3,19 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Bualoi-s-Dev/backend/models"
 	"github.com/tkrajina/typescriptify-golang-structs/typescriptify"
 )
 
 func main() {
-	outputFile := "api_types.ts"
+	outputFile := "gen/api_types.ts"
 	converter := typescriptify.New()
 
+	// Add models
 	converter.Add(models.Package{})
 
+	// Change to interface
 	converter.CreateInterface = true
 
+	// Create dir
+	dirErr := os.MkdirAll("gen", os.ModePerm)
+	if dirErr != nil {
+		log.Fatalf("Error creating directory: %v", dirErr)
+		return
+	}
+
+	// Convert
 	err := converter.ConvertToFile(outputFile)
 
 	if err != nil {
