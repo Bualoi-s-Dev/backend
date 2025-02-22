b6008e4c7438b23673db88aeaee3399e3cc6b3c3
ThanaphomH <thanaphom.h@gmail.com>
2025-02-22 18:17:06 +0700

    feat: basic model of subpackage

---
+++ models/subpackage_model.go
@@ -0,0 +1,45 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Subpackage struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id" ts_type:"string" example:"12345678abcd"`
+	PackageID   primitive.ObjectID `bson:"package_id,omitempty" json:"packageId" ts_type:"string" example:"12345678abcd"`
+	Title       string             `bson:"title,omitempty" json:"title" example:"Wedding Bliss Package"`
+	Description string             `bson:"description,omitempty" json:"description" example:"This is a package for wedding"`
+	Duration    int                `bson:"duration,omitempty" json:"duration" example:"60" description:"Duration in minutes"`
+	Price       int                `bson:"price,omitempty" json:"price" example:"10000"`
+
+	IsInf              bool      `bson:"is_inf,omitempty" json:"isInf" example:"false"`
+	RepeatedDay        []DayName `bson:"repeated_day,omitempty" json:"repeatedDay" binding:"day_names" example:"MON,TUE,WED"`
+	AvaliableStartTime string    `bson:"avaliable_start_time,omitempty" json:"avaliableStartTime" binding:"time_format" example:"15:04"`
+	AvaliableEndTime   string    `bson:"avaliable_end_time,omitempty" json:"avaliableEndTime" binding:"time_format" example:"16:27"`
+
+	AvaliableStartDay string `bson:"avaliable_start_day,omitempty" json:"avaliableStartDay" binding:"date_format" example:"2021-01-01"`
+	AvaliableEndDay   string `bson:"avaliable_end_day,omitempty" json:"avaliableEndDay" binding:"date_format" example:"2021-12-31"`
+}
+
+type DayName string
+
+const (
+	Sunday    DayName = "SUN"
+	Monday    DayName = "MON"
+	Tuesday   DayName = "TUE"
+	Wednesday DayName = "WED"
+	Thursday  DayName = "THU"
+	Friday    DayName = "FRI"
+	Saturday  DayName = "SAT"
+)
+
+var ValidDayNames = []struct {
+	Value  DayName
+	TSName string
+}{
+	{Sunday, string(Sunday)},
+	{Monday, string(Monday)},
+	{Tuesday, string(Tuesday)},
+	{Wednesday, string(Wednesday)},
+	{Thursday, string(Thursday)},
+	{Friday, string(Friday)},
+	{Saturday, string(Saturday)},
+}
