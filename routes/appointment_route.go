6eb75054bf92139c3305f6464ee4e38b5169f31c
Pana Wanitchollakit <pnaw.wan@gmail.com>
2025-02-23 12:31:43 +0700

    fix: appointment routing

--- routes/appointment_route.go
+++ routes/appointment_route.go
@@ -1,21 +1,17 @@
 package routes
 
 import (
-	"os"
-
 	"github.com/Bualoi-s-Dev/backend/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func AppointmentRoutes(router *gin.Engine, ctrl *controllers.AppointmentController) {
-	if os.Getenv("APP_MODE") == "development" {
-		appointmentGroup := router.Group("/appointment")
+	appointmentGroup := router.Group("/appointment")
 
-		appointmentGroup.GET("", ctrl.GetAllAppointment)
-		appointmentGroup.GET("/:id", ctrl.GetAppointmentById)
-		appointmentGroup.POST("", ctrl.CreateAppointment)
-		appointmentGroup.PATCH("/:id", ctrl.UpdateAppointment)
-		appointmentGroup.PATCH("/status/:id", ctrl.UpdateAppointmentStatus)
-		appointmentGroup.DELETE("/:id", ctrl.DeleteAppointment)
-	}
+	appointmentGroup.GET("", ctrl.GetAllAppointment)
+	appointmentGroup.GET("/:id", ctrl.GetAppointmentById)
+	appointmentGroup.POST("", ctrl.CreateAppointment)
+	appointmentGroup.PATCH("/:id", ctrl.UpdateAppointment)
+	appointmentGroup.PATCH("/status/:id", ctrl.UpdateAppointmentStatus)
+	appointmentGroup.DELETE("/:id", ctrl.DeleteAppointment)
 }
