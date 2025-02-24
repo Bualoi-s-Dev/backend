8545c586a0d94af640eb9e376ac723dfacb8117a
Pana Wanitchollakit <pnaw.wan@gmail.com>
2025-02-24 20:24:16 +0700

    feat: add new appointment status (canceled)

--- models/appointment_model.go
+++ models/appointment_model.go
@@ -6,12 +6,12 @@ import (
 
 type Appointment struct {
 	ID             primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty" example:"656e2b5e3f1a3c4d8b9e1234"`
-	CustomerID     primitive.ObjectID `bson:"customer_id" json:"customer_id" example:"656e2b5e3f1a3c4d8b9e1238"`
-	PhotographerID primitive.ObjectID `bson:"photographer_id" json:"photographer_id" example:"656e2b5e3f1a324d8b9e1236"`
-	PackageID      primitive.ObjectID `bson:"package_id" json:"package_id" example:"656e2b5e3f1a3c4d8b9e1235"`
-	SubPackageID   primitive.ObjectID `bson:"sub_package_id" json:"sub_package_id" example:"656e2b5e3f1a3c4d8b9e9999"`
-	BusyTimeID     primitive.ObjectID `bson:"busy_time_id" json:"busy_time_id" example:"656e2b5e3f1a3c4d8b9e7877"`
-	Status         AppointmentStatus  `bson:"status" json:"status" example:"pending"` // "pending", "accepted", "rejected", "completed"
+	CustomerID     primitive.ObjectID `bson:"customer_id" json:"customerId" example:"656e2b5e3f1a3c4d8b9e1238"`
+	PhotographerID primitive.ObjectID `bson:"photographer_id" json:"photographerId" example:"656e2b5e3f1a324d8b9e1236"`
+	PackageID      primitive.ObjectID `bson:"package_id" json:"packageId" example:"656e2b5e3f1a3c4d8b9e1235"`
+	SubPackageID   primitive.ObjectID `bson:"sub_package_id" json:"subPackageId" example:"656e2b5e3f1a3c4d8b9e9999"`
+	BusyTimeID     primitive.ObjectID `bson:"busy_time_id" json:"busyTimeId" example:"656e2b5e3f1a3c4d8b9e7877"`
+	Status         AppointmentStatus  `bson:"status" json:"status" example:"pending"`
 	Location       string             `bson:"location,omitempty" json:"location" example:"Bangkok, Thailand"`
 	// Payment       Payment            `bson:"payment,omitempty" json:"payment,omitempty" example:"{...}"`
 }
@@ -19,18 +19,20 @@ type Appointment struct {
 type AppointmentStatus string
 
 const (
-	Pending   AppointmentStatus = "Pending"
-	Accepted  AppointmentStatus = "Accepted"
-	Rejected  AppointmentStatus = "Rejected"
-	Completed AppointmentStatus = "Completed"
+	AppointmentPending   AppointmentStatus = "Pending"
+	AppointmentAccepted  AppointmentStatus = "Accepted"
+	AppointmentRejected  AppointmentStatus = "Rejected"
+	AppointmentCanceled  AppointmentStatus = "Canceled"
+	AppointmentCompleted AppointmentStatus = "Completed"
 )
 
 var ValidAppointmentStatus = []struct {
 	Value  AppointmentStatus
 	TSName string
 }{
-	{Pending, string(Pending)},
-	{Accepted, string(Accepted)},
-	{Rejected, string(Rejected)},
-	{Completed, string(Completed)},
+	{AppointmentPending, string(AppointmentPending)},
+	{AppointmentAccepted, string(AppointmentAccepted)},
+	{AppointmentRejected, string(AppointmentRejected)},
+	{AppointmentCanceled, string(AppointmentCanceled)},
+	{AppointmentCompleted, string(AppointmentCompleted)},
 }
--- models/busy_time_model.go
+++ models/busy_time_model.go
@@ -1,16 +1,18 @@
 package models
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type BusyTime struct {
-	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id" ts_type:"string" example:"12345678abcd"`
-	PhotographerID  primitive.ObjectID `bson:"photographer_id,omitempty" json:"photographerId" ts_type:"string" example:"12345678abcd"`
-	Type            BusyTimeType       `bson:"type,omitempty" json:"type" binding:"omitempty,busy_time_type" example:"Photographer"`
-	StartTime       time.Time          `bson:"start_time,omitempty" json:"startTime" example:"2025-02-23T10:00:00Z"`
-	EndTime         time.Time          `bson:"end_time,omitempty" json:"endTime" example:"2025-02-23T12:00:00Z"`
+	ID             primitive.ObjectID `bson:"_id,omitempty" json:"id" ts_type:"string" example:"12345678abcd"`
+	PhotographerID primitive.ObjectID `bson:"photographer_id,omitempty" json:"photographerId" ts_type:"string" example:"12345678abcd"`
+	Type           BusyTimeType       `bson:"type,omitempty" json:"type" binding:"omitempty,busy_time_type" example:"Photographer"`
+	StartTime      time.Time          `bson:"start_time,omitempty" json:"startTime" example:"2025-02-23T10:00:00Z"`
+	EndTime        time.Time          `bson:"end_time,omitempty" json:"endTime" example:"2025-02-23T12:00:00Z"`
+	IsValid        bool               `bson:"is_valid,omitempty" json:"isValid" example:"true"`
 }
 
 type BusyTimeType string
