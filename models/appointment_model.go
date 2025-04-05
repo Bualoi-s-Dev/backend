197578b70117f56c4cbfb4e3645157a1ec697b16
Pana Wanitchollakit <pnaw.wan@gmail.com>
2025-04-05 14:47:51 +0700

    fix: run

--- models/appointment_model.go
+++ models/appointment_model.go
@@ -9,8 +9,8 @@ type Appointment struct {
 	ID             primitive.ObjectID `bson:"_id,omitempty" json:"id" ts_type:"string" example:"656e2b5e3f1a3c4d8b9e1234"`
 	CustomerID     primitive.ObjectID `bson:"customer_id" json:"customerId" ts_type:"string" example:"656e2b5e3f1a3c4d8b9e1238"`
 	PhotographerID primitive.ObjectID `bson:"photographer_id" json:"photographerId" ts_type:"string" example:"656e2b5e3f1a324d8b9e1236"`
-	Package        Package            `bson:"package" json:"package" ts_type:"models.package" example:"{}"`
-	Subpackage     Subpackage         `bson:"sub_package" json:"subpackage" ts_type:"models.subpackage" example:"{}"`
+	Package        Package            `bson:"package" json:"package" ts_type:"models.package" example:"{...}"`
+	Subpackage     Subpackage         `bson:"sub_package" json:"subpackage" ts_type:"models.subpackage" example:"{...}"`
 	BusyTimeID     primitive.ObjectID `bson:"busy_time_id" json:"busyTimeId" ts_type:"string" example:"656e2b5e3f1a3c4d8b9e7877"`
 	Status         AppointmentStatus  `bson:"status" json:"status" binding:"appointment_status" ts_type:"string" example:"pending"`
 	Location       string             `bson:"location,omitempty" json:"location" ts_type:"string" example:"Bangkok, Thailand"`
