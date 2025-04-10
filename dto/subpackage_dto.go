6787a8fa794a49585e338b924f11dd8c7f16c5ea
ThanaphomH <thanaphom.h@gmail.com>
2025-04-10 10:39:12 +0700

    fix: change ts_type from string to enum

--- dto/subpackage_dto.go
+++ dto/subpackage_dto.go
@@ -12,9 +12,8 @@ type SubpackageRequest struct {
 	Price       *int    `bson:"price" json:"price" binding:"omitempty" example:"10000"`
 	Duration    *int    `bson:"duration" json:"duration" binding:"omitempty" example:"60" description:"Duration in minutes"`
 
-	IsInf *bool `bson:"is_inf" json:"isInf" binding:"omitempty,isInf_rule" example:"false"`
-	// TODO: change tsgen type to Dayname
-	RepeatedDay        *[]models.DayName `bson:"repeated_day" json:"repeatedDay" binding:"omitempty,day_names" example:"MON,TUE,WED"`
+	IsInf              *bool             `bson:"is_inf" json:"isInf" binding:"omitempty,isInf_rule" example:"false"`
+	RepeatedDay        *[]models.DayName `bson:"repeated_day" json:"repeatedDay" binding:"omitempty,day_names" ts_type:"DayName" example:"MON,TUE,WED"`
 	AvailableStartTime *string           `bson:"available_start_time" json:"availableStartTime" binding:"omitempty,time_format" example:"15:04"`
 	AvailableEndTime   *string           `bson:"available_end_time" json:"availableEndTime" binding:"omitempty,time_format" example:"16:27"`
 	AvailableStartDay  *string           `bson:"available_start_day" json:"availableStartDay" binding:"omitempty,date_format" example:"2021-01-01"`
@@ -29,9 +28,8 @@ type SubpackageResponse struct {
 	Price       int                `bson:"price" json:"price" binding:"omitempty" example:"10000"`
 	Duration    int                `bson:"duration" json:"duration" binding:"omitempty" example:"60" description:"Duration in minutes"`
 
-	IsInf bool `bson:"is_inf" json:"isInf" binding:"omitempty,isInf_rule" example:"false"`
-	// TODO: change tsgen type to Dayname
-	RepeatedDay        []models.DayName `bson:"repeated_day" json:"repeatedDay" binding:"omitempty,day_names" example:"MON,TUE,WED"`
+	IsInf              bool             `bson:"is_inf" json:"isInf" binding:"omitempty,isInf_rule" example:"false"`
+	RepeatedDay        []models.DayName `bson:"repeated_day" json:"repeatedDay" binding:"omitempty,day_names" ts_type:"DayName" example:"MON,TUE,WED"`
 	AvailableStartTime string           `bson:"available_start_time" json:"availableStartTime" binding:"omitempty,time_format" example:"15:04"`
 	AvailableEndTime   string           `bson:"available_end_time" json:"availableEndTime" binding:"omitempty,time_format" example:"16:27"`
 	AvailableStartDay  string           `bson:"available_start_day" json:"availableStartDay" binding:"omitempty,date_format" example:"2021-01-01"`
